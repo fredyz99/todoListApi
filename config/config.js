36fc17627b71e47dc20e7d673a683a07f25d0564
fredyz99 <52936217+fredyz99@users.noreply.github.com>
2020-02-08 14:33:31 +0700

    add config

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 modules.exports = {
     DB: process.env.HOST ? process.env.HOST : 'localhost',
-    APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : 80,
+    APP_PORT: process.env.APP_PORT ? process.env.PORT : 80,
 };
